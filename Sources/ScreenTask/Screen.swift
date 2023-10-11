5c5b37baac82b1bf6ba4c325431b1bf656d99701
VincentDreem <vincent.lemonnier@dreem.com>
2023-10-11 15:41:24 +0200

    [Sources] Added property Screen.transition

--- Sources/ScreenTask/Screen.swift
+++ Sources/ScreenTask/Screen.swift
@@ -5,7 +5,7 @@ internal class Screen: UIHostingController<AnyView>, Identifiable {
     let name: String
     let backgroundColor: Color
     var onNavigateTo: ([String : Any]) -> Void = { _ in }
-
+    
     init(
         name: String,
         backgroundColor: Color,
@@ -23,5 +23,37 @@ internal class Screen: UIHostingController<AnyView>, Identifiable {
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
         self.view.backgroundColor = self.backgroundColor.uiColor
+    }
+    
+    var transition: Transition = .none {
+        willSet {
+            self.view.backgroundColor = self.backgroundColor.uiColor
+            self.modalTransitionStyle = .coverVertical
+            self.transitioningDelegate = nil
+            let presentationStyle: UIModalPresentationStyle
+            switch newValue {
+            case .none:
+                presentationStyle = .fullScreen
+            case .coverFullscreen:
+                modalTransitionStyle = .crossDissolve
+                presentationStyle = .fullScreen
+            case .coverOverFullscreen:
+                view.backgroundColor = nil
+                modalTransitionStyle = .crossDissolve
+                presentationStyle = .overFullScreen
+            case .coverVertical:
+                modalTransitionStyle = .coverVertical
+                presentationStyle = .fullScreen
+            case .coverHorizontal:
+                presentationStyle = .custom
+                transitioningDelegate = TransitionDelegate.coverHorizontal
+            case .sheet:
+                modalTransitionStyle = .coverVertical
+                presentationStyle = .pageSheet
+            }
+            if presentingViewController == nil {
+                modalPresentationStyle = presentationStyle
+            }
+        }
     }
 }
